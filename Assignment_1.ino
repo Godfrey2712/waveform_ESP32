2fe7177fee7c65fc2eacbb02b4dff3b776d29fc9
Godfrey <godfreyinyama@gmail.com>
2022-02-08 20:56:56 +0000

    Comments

--- Assignment_1.ino
+++ Assignment_1.ino
@@ -1,19 +1,20 @@
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin1 = 22;     // the number of the pushbutton pin 1
-const int ledPin1 = 15;      // the number of the LED pin 1
-const int buttonPin2 = 23;     // the number of the pushbutton pin 2
-const int ledPin2 = 21;      // the number of the LED pin 2
-#define a 1000
-#define b 1300
-#define c 6
-#define d 5000
-#define a1 5000
-#define a2 10000
-#define B 500 //Signal of B
+const int buttonPin1 = 22;     // the number of the pushbutton pin 1 used to enable or disable the wave form
+const int ledPin1 = 15;        // the number of the LED pin 1 for Signal A
+const int buttonPin2 = 23;     // the number of the pushbutton pin 2 used to switch modes
+const int ledPin2 = 21;        // the number of the LED pin 2 for Signal B
+
+// variables after calculations using my surname and formulars given:
+#define a 1000    //1st pulse from signal A
+#define b 1300    //width of space between pulses
+#define c 6       //Number of pulses in a block
+#define d 5000    //space between pulse blocks
+#define a1 5000   //2nd pulse from signal A
+#define a2 10000  //3rd pulse from signal A
+#define B 500     //Time of Signal B being in the HIGH state
 
 // variables will change:
 int buttonState = 1;         // variable for reading the pushbutton1 status
-//int buttonState2 = 0;         // variable for reading the pushbutton2 status
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,19 +24,14 @@ void setup() {
   //initialize the switches as inputs
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
-
-
 }
 
 void loop() {
-      buttonState = digitalRead(buttonPin1);
+       buttonState = digitalRead(buttonPin1);
    if (buttonState == LOW && digitalRead(buttonPin2) == LOW) {
-      //digitalWrite(ledPin1, HIGH);
       digitalWrite(ledPin2, HIGH);
       delay(B);
-      //digitalWrite(ledPin1, LOW);
       digitalWrite(ledPin2, LOW);
-      //delay(50);
       digitalWrite(ledPin1, HIGH);
       delay(a);
       digitalWrite(ledPin1,LOW);
